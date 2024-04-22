d50104a38ebbde8da468df91c786ad6673353fef
ea-corp <167676098+ea-corp@users.noreply.github.com>
2024-04-22 20:16:37 +0000

    Add reviews on hidden pages

--- app/(landing)/remote-services/(routes)/pricing-usd/page.tsx
+++ app/(landing)/remote-services/(routes)/pricing-usd/page.tsx
@@ -2,6 +2,8 @@ import { Separator } from "@/components/ui/separator";
 import { Check, Package, ScreenShare } from "lucide-react";
 import { Metadata } from "next";
 import Faq from "../../_components/faq";
+import { ReviewCard } from "@/app/(landing)/_components/reviews";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 
 export const metadata: Metadata = {
   title: "Services - Workflow Made Easy",
@@ -65,6 +67,60 @@ export default function ServicePage() {
       ],
     },
   ];
+
+  const reviews = [
+    {
+      projectName: 'Project: monday.com Work Flow optimization',
+      rating: 5,
+      reviewText: 'Eddy has been integral to the growth of our business! His insight, dedication, and support on Monday.com have made Eddy a key part of our team. I wouldn’t hesitate to recommend him for any process-related projects. A++',
+      sourceLogo: '/images/upwork-logo.png',
+    },
+    {
+      projectName: 'Project: monday Scorecard',
+      rating: 5,
+      reviewText: "Eddy is a Monday expert and an absolutely phenomenal partner to work with. I can't recommend him enough. He will execute any ideas/solutions you have beyond your best expectations.",
+      sourceLogo: '/images/upwork-logo.png',
+    },
+    {
+      projectName: 'Project: monday.com setup',
+      rating: 5,
+      reviewText: "Eddy is a real monday.com master. Knows what he's doing, responsive and delivers. What more could we have asked for...",
+      sourceLogo: '/images/upwork-logo.png',
+    },
+    {
+      projectName: 'Project: monday.com expert needed for fine tuning & training',
+      rating: 5,
+      reviewText: "Eddy is great! He is extremely knowledgeable in the monday.com software. He always made the effort to fit me in last minute, and took the time to explain each process with me when I didn't understand something. He has helped me build complicated automations and gave me suggestions to improve my current boards. I will be hiring Eddy again in the future whenever I need further help to develop my monday boards. Thanks Eddy!",
+      sourceLogo: '/images/upwork-logo.png',
+    },
+    {
+      projectName: 'Project: monday.com Training For Small Business',
+      rating: 5,
+      reviewText: "Eddy did an amazing job at training our team on how to use Monday.com. He is extremely professional, knowledgeable and communicates really well. We will definitely be hiring him in the future for any further training. Thanks Eddy!",
+      sourceLogo: '/images/upwork-logo.png',
+    },
+    {
+      projectName: 'Project: Monday.com expert need to automate, optimize and integrate platform',
+      rating: 5,
+      reviewText: "Eddy is a real monday.com master. Knows what he's doing, responsive and delivers. What more could we have asked for....",
+      sourceLogo: '/images/upwork-logo.png',
+    },
+    {
+      projectName: 'Project: monday.com setup',
+      rating: 5,
+      reviewText:"I hired Eddy to help me out with setting up Monday.com. He was super helpful, and a really great hire. I highly recommend him. Eddy is exceptionally well informed about Monday.com, I was impressed by how up-to-date his knowledge is. He was even able to tell me what features are coming soon in updates. He was easy to work with, patient, and a very good communicator. He answered all my questions and showed me some things I didn't even know I needed to know. I'm very glad that I hired him, I feel that our brief time together saved me hours of trying to figure things out on my own. I do look forward to working with him again, as my Monday.com needs grow.",
+      sourceLogo: '/images/upwork-logo.png',
+    },
+    {
+      projectName: 'Project: monday.com expert needed for growing agency',
+      rating: 5,
+      reviewText: "Excellent work and highly recommend Eddy! He was extremely knowledgeable about monday.com, very responsive with excellent communication. He understood our needs and quickly revamped our workflow. We will definitely continue an ongoing relationship with him. Thank you, Eddy - GREAT JOB!",
+      sourceLogo: '/images/upwork-logo.png',
+    },
+
+
+  ];
+
 
   return (
     <div>
@@ -158,6 +214,26 @@ export default function ServicePage() {
           </div>
         ))}
       </div>
+
+
+
+      <div className="flex items-center justify-center mt-16">
+    <Carousel className="w-10/12">
+      <CarouselContent>
+        {reviews.map((review, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <ReviewCard {...review} />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious aria-label="Previous review" />
+      <CarouselNext aria-label="Next review" />
+    </Carousel>  
+</div>
+
+
       <Faq />
       <div className="bg-gray-100 py-16 flex items-center justify-center flex-col">
         <h3 className="text-2xl font-medium py-4">
