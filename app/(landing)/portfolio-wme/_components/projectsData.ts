e72b8845f993eacba2a3b57381a4f3e8a1b68f95
qentsolutions <contact@qentsolutions.com>
2025-03-24 20:44:11 +0100

    Add new portfolio project

--- app/(landing)/portfolio-wme/_components/projectsData.ts
+++ app/(landing)/portfolio-wme/_components/projectsData.ts
@@ -67,14 +67,16 @@ export const projectsData = [
   {
     id: 9,
     title: "QR Codes Scan",
-    description:"Scanning QR codes at the Factory changing automatically statuses and timers on the Monday board.",
+    description:
+      "Scanning QR codes at the Factory changing automatically statuses and timers on the Monday board.",
     image: "/images/portfolio/manufacturing/image-1.png",
     tags: ["Manufacturing", "Integrations"],
   },
   {
     id: 10,
     title: "Sample Manufacturing Board 1",
-    description:"Orders status for each station changing after the employees canning the orders at the factory.",
+    description:
+      "Orders status for each station changing after the employees canning the orders at the factory.",
     image: "/images/portfolio/manufacturing/image-2.png",
     tags: ["Manufacturing", "Operations/Checklist", "Time tracking"],
   },
@@ -102,14 +104,16 @@ export const projectsData = [
   {
     id: 14,
     title: "Live Dashboard - Daily Deliveries",
-    description:  "Tracking orders made by customers on different channels. Reporting was done daily, weekly and monthly. Reporting was made per location as this restaurant had several branches.",
+    description:
+      "Tracking orders made by customers on different channels. Reporting was done daily, weekly and monthly. Reporting was made per location as this restaurant had several branches.",
     image: "/images/portfolio/restaurant/image-1.png",
     tags: ["Restaurant", "Dashboard", "Orders tracking"],
   },
   {
     id: 15,
     title: "Dashboard 2",
-    description:"We were able to tracking on the 2 bottom widgets the orders by geographical zones, and by type of customers",
+    description:
+      "We were able to tracking on the 2 bottom widgets the orders by geographical zones, and by type of customers",
     image: "/images/portfolio/restaurant/image-2.png",
     tags: ["Restaurant", "Dashboard", "Orders tracking"],
   },
@@ -160,83 +164,61 @@ export const projectsData = [
   },
   {
     id: 22,
-    title: 
-      "Sample Automations 2",
-    description: 
-      "",
+    title: "Sample Automations 2",
+    description: "",
     image: "/images/portfolio/real-estate/image-5.png",
     tags: ["Real Estate", "Automations"],
   },
   // Marketing
   {
     id: 26,
-    title: 
-      "Dashboard Sample 1- Client Overview",
-    description: 
+    title: "Dashboard Sample 1- Client Overview",
+    description:
       "Used by a Marketing Agency to see what are the activity for a particular account. The leads are connected from the client's website so that everything can be managed within Monday.com",
     image: "/images/portfolio/marketing-agency/image-1.png",
     tags: ["Marketing", "Dashboard"],
   },
   {
     id: 27,
-    title: 
-      "Sample Board 1- Marekting Agency",
-    description: 
-      "Sample Board 1- Marketing Agency",
+    title: "Sample Board 1- Marekting Agency",
+    description: "Sample Board 1- Marketing Agency",
     image: "/images/portfolio/marketing-agency/image-2.png",
     tags: ["Marketing", "Project Management"],
   },
   {
     id: 28,
-    title: 
-      "Sample Board 2- Video Production",
-    description: 
-      "",
+    title: "Sample Board 2- Video Production",
+    description: "",
     image: "/images/portfolio/marketing-agency/image-3.png",
     tags: ["Marketing", "Project Management"],
   },
   {
     id: 29,
-    title: 
-      "Sample Board 3- Video Production",
-    description: 
-      "",
+    title: "Sample Board 3- Video Production",
+    description: "",
     image: "/images/portfolio/marketing-agency/image-4.png",
     tags: ["Marketing", "Project Management"],
   },
   {
     id: 30,
-    title: 
-      "Sample Automations 1",
-    description: 
-      "",
+    title: "Sample Automations 1",
+    description: "",
     image: "/images/portfolio/marketing-agency/image-5.png",
     tags: ["Marketing", "Automations"],
   },
   // Timetracking
   {
     id: 31,
-    title: 
-      "Time tracking report on Toggl integrated with Monday",
+    title: "Time tracking report on Toggl integrated with Monday",
     description: "",
     image: "/images/portfolio/time-tracking/image-1.png",
-    imagesCarousel: 
-      ["images/portfolio/time-tracking/image-1.png"],
-    tags: ["Marketing", "Time tracking"],
-  },
-  {
-    id: 32,
-    title: 
-      "Time tracking report on Toggl integrated with Monday",
-    description: "",
-    image: "/images/portfolio/time-tracking/image-2.png",
+    imagesCarousel: ["images/portfolio/time-tracking/image-1.png"],
     tags: ["Marketing", "Time tracking"],
   },
   {
     id: 33,
-    title: 
-      "2 Way Sync of the tasks between Monday and Toggl",
-      
+    title: "2 Way Sync of the tasks between Monday and Toggl",
+
     description: "",
     image: "/images/portfolio/time-tracking/image-3.png",
 
@@ -244,11 +226,224 @@ export const projectsData = [
   },
   {
     id: 34,
-    title: 
-      "Sample o the scenario we have build using Make.com",
+    title: "Sample o the scenario we have build using Make.com",
     description: "",
     image: "/images/portfolio/time-tracking/image-4.png",
-   
+
     tags: ["Marketing", "Time tracking", "Make.com"],
+  },
+  {
+    id: 35,
+    title: "Accounts Board",
+    description:
+      "The Accounts board tracks organizations, their details, and links them to opportunities. It also tracks projects that are associated with the different organizations.",
+    image: "/images/portfolio/manufacturing/image-6.png",
+
+    tags: ["Manufacturing"],
+  },
+  {
+    id: 36,
+    title: "Opportunities Board",
+    description:
+      'The Opportunities board records new opportunities, associating them with accounts to streamline tracking. The sales team can create new projects in the different salesperson workspace when an opportunity reaches the "Take Off" stage.',
+    image: "/images/portfolio/manufacturing/image-7.png",
+
+    tags: ["Manufacturing"],
+  },
+  {
+    id: 37,
+    title: "Project Summary Board",
+    description:
+      "The Project Summary board provides the sales team with an overview of ongoing projects for better management and visibility.",
+    image: "/images/portfolio/manufacturing/image-8.png",
+
+    tags: ["Manufacturing"],
+  },
+  {
+    id: 38,
+    title: "Project Board",
+    description:"The Project board provides the detailed list of tasks the salesperson should follow during the project implementation process.",
+    image: "/images/portfolio/manufacturing/image-9.png",
+
+    tags: ["Manufacturing", "Project Management"],
+  },
+  {
+    id: 39,
+    title: "Quote Generation",
+    description : "The Opportunity Board with an item card for Quote Generation that allows users to create invoices and quotes directly from opportunities. Once an opportunity reaches a Proposal stage, users can generate a document with pre-filled details like job number, account, and pricing. Users can add specific requests from clients for different items in the item card. This streamlines the sales process, ensuring quick and accurate quote creation.",
+    image: "/images/portfolio/manufacturing/image-10.png",
+
+    tags: ["Manufacturing", "Invoicing", "Docgeneration"],
+  },
+  {
+    id: 40,
+    title: "Quote Document",
+    description:"The generated document in Docugen using the prefill fields from the Opportunities board.",
+    image: "/images/portfolio/manufacturing/image-11.png",
+
+    tags: ["Manufacturing", "Invoicing", "Docgeneration"],
+  },
+  {
+    id: 41,
+    title: "Invoice Generation Retail 1/3",
+    description:"The Monday CRM Quotes & Invoices feature allows users to easily create quotes and invoices by connecting their client list and product catalog to the invoice templates. This streamlines the billing process, saving time and ensuring accuracy. It simplifies client management with seamless integration of client and product data.",
+    image: "/images/portfolio/retail/image-1.png",
+
+    tags: ["Retail", "CRM", "Docgeneration"],
+  },
+  {
+    id: 42,
+    title: "Invoice Generation Retail 2/3",
+    description:"The Monday CRM Quotes & Invoices feature allows users to easily create quotes and invoices by connecting their client list and product catalog to the invoice templates. This streamlines the billing process, saving time and ensuring accuracy. It simplifies client management with seamless integration of client and product data.",
+    image: "/images/portfolio/retail/image-2.png",
+
+    tags: ["Retail", "CRM", "Docgeneration"],
+  },
+  {
+    id: 43,
+    title: "Invoice Generation Retail 3/3",
+    description:"The Monday CRM Quotes & Invoices feature allows users to easily create quotes and invoices by connecting their client list and product catalog to the invoice templates. This streamlines the billing process, saving time and ensuring accuracy. It simplifies client management with seamless integration of client and product data.",
+    image: "/images/portfolio/retail/image-3.png",
+
+    tags: ["Retail", "Invoicing", "CRM", "Docgeneration"],
+  },
+  {
+    id: 44,
+    title: "Invoice Document",
+    description:"The generated document using the Monday Quotes & Invoice feature.",
+    image: "/images/portfolio/retail/image-4.png",
+
+    tags: ["Retail", "Invoicing", "CRM", "Docgeneration"],
+  },
+  {
+    id: 45,
+    title: "Organization Activity Summary",
+    description:"The Organisation Snapshot Dashboard provides an overview of activities such as emails, events, meetings, and calls, filtered by organisation. It also tracks awards given to organisations, displaying categories like wins, shortlisted entries, highly commended, and those that didn't win.",
+    image: "/images/portfolio/activities-summary.png",
+
+    tags: ["Activity Tracking"],
+  },
+  {
+    id: 46,
+    title: "Activity Board",
+    description:"The Activities board provides a centralized view of all activities, tracking both emails, meetings and call summaries for different organizations in one place.It allows users to stay updated on communication and task progress efficiently.",
+    image: "/images/portfolio/real-estate/image-6.png",
+
+    tags: ["Real Estate", "Activity Tracking"],
+  },
+  {
+    id: 47,
+    title: "Activity Summary Chart",
+    description:"Activity summary chart provides a visual representation of the activities for the organisation. This enhances workflow visibility and helps manage engagements seamlessly.",
+    image: "/images/portfolio/real-estate/image-7.png",
+
+    tags: ["Real Estate", "Activity Tracking"],
+  },
+  {
+    id: 48,
+    title: "Emails & Activities",
+    description:"Emails and Activities logs all activities associated with the email column on the board. For example, you can send emails through this page to the contacts, add meeting notes and call summery as well as create custom activities such as sales performance analysis, and contract negotiations so you can add all the necessary information associated to this contact.",
+    image: "/images/portfolio/real-estate/image-8.png",
+
+    tags: ["Real Estate", "Activity Tracking"],
+  },
+  {
+    id: 49,
+    title: "Activity Tracker",
+    description : "The Activity Tracker tracks emails and other activities, displaying them under the name of the person who added them. It provides an overview of various activities added in the Emails and Activities such as emails, notes, call summery as well as the custom activities such as sales performance analysis, and contract negotiations. Any new activity logged will instantly reflect in the chart.",
+    image: "/images/portfolio/real-estate/image-8.png",
+
+    tags: ["Real Estate", "Activity Tracking"],
+  },
+  {
+    id: 50,
+    title: "Email & Activities Timeline",
+    description : "Emails & Activities Timeline tracks all activities for the past 45 days, displaying email interactions and activities such as meetings, notes, and custom activities that is added in the Emails & Activities item card. Daily activity is color-coded with pink representing incoming emails and blue for all other activities. This visual system helps users easily identify and manage communication flow and task progress.",
+    image: "/images/portfolio/real-estate/image-9.png",
+    tags: ["Real Estate", "Activity Tracking"],
+  },
+  {
+    id: 51,
+    title: "Lead Tracking Board",
+    description : "This custom CRM is built for a construction and real estate company which streamlines lead tracking, including their sources, viewing follow-ups, and more.",
+    image: "/images/portfolio/real-estate/image-10.png",
+    tags: ["Real Estate", "CRM"],
+  },
+  {
+    id: 52,
+    title: "Viewings Board for Efficient Scheduling ",
+    description : "The viewings board is part of the CRM that enables the sales team to schedule unit viewings with leads and efficiently manage their schedule.",
+    image: "/images/portfolio/real-estate/image-11.png",
+    tags: ["Real Estate", "CRM"],
+  },
+  {
+    id: 53,
+    title: "Opportunity Tracking & Contract generation Board",
+    description: "The Opportunity board allows you to track all opportunities within your business, set contract terms, generate contracts, and send them electronically for signature.",
+    image: "/images/portfolio/real-estate/image-12.png",
+    tags: ["Real Estate", "CRM"]
+  },
+  {
+    id: 54,
+    title: "Units Occupancy & Lease Tracking Board",
+    description: "The Units board is designed to track the occupancy of units across different sites. It effectively monitors occupancy, as well as current and past tenant information. A key feature of this board is the automatic lease renewal reminders for the sales team, ensuring timely follow-up with tenants to either end or renew their contracts.",
+    image: "/images/portfolio/real-estate/image-13.png",
+    tags: ["Real Estate", "CRM"]
+  },
+  {
+    id: 55,
+    title: "Lead Analysis Dashboard (1)",
+    description: "This dashboard provides a clear overview of sales performance, total leads, conversion rates, and contracts.",
+    image: "/images/portfolio/real-estate/image-14.png",
+    tags: ["Real Estate", "Dashboard"]
+  },
+  {
+    id: 56,
+    title: "Lead Analysis Dashboard (2)",
+    description: "This dashboard tracks inquiry types, viewing bookings, contract signings, and more, providing valuable insights to support better decision-making.",
+    image: "/images/portfolio/real-estate/image-15.png",
+    tags: ["Real Estate", "Dashboard"]
+  },
+  {
+    id: 57,
+    title: "I AM Meter Integration for Automated Energy Tracking",
+    description: "Successfully integrated the I AM Meter platform with Monday.com, enabling both daily and monthly synchronization of electricity usage for all units. This ensures more accurate reporting and automates electricity bill generation and delivery to tenants.",
+    image: "/images/portfolio/construction/image-4.png",
+    tags: ["Construction", "Integrations"]
+  },
+  {
+    id: 58,
+    title: "Monthly Tax Tracking Board (1) ",
+    description: "This board was designed to track taxes on a monthly basis. The data is automatically rolled over, enabling the team to work more efficiently.",
+    image: "/images/portfolio/accounting/image-1.png",
+    tags: ["Accounting", "Integrations"]
+  },
+  {
+    id: 59,
+    title: "Monthly Tax Tracking Board (2) ",
+    description: "This board was designed to track taxes on a monthly basis. The data is automatically rolled over, enabling the team to work more efficiently.",
+    image: "/images/portfolio/accounting/image-2.png",
+    tags: ["Accounting", "Integrations"]
+  },
+  {
+    id: 60,
+    title: "Centralized Tax Rollover for 800+ Boards",
+    description: "Triggering the tax rollover process for over 800 boards from a high-level board that manages all clients.",
+    image: "/images/portfolio/accounting/image-3.png",
+    tags: ["Accounting", "Integrations"]
+  },
+  {
+    id: 61,
+    title: "Automated Tax Rollover with Make.com",
+    description: "This Make.com scenario enables, with a single click, the rollover of previous tax balances, preparation of upcoming months. This solution streamlines workflows, reduces errors, and enhances efficiency.",
+    image: "/images/portfolio/accounting/image-4.png",
+    tags: ["Accounting", "Make.com"]
+  },
+  {
+    id: 62,
+    title: "Automated Data Archiving with Make.com",
+    description: "This Make.com scenario archives data, keeping it accessible to the team while locking it for edits.",
+    image: "/images/portfolio/accounting/image-5.png",
+    tags: ["Accounting", "Make.com"]
   },
 ];
